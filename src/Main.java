6b609cc6cff575c8b51ee619437eaf25a1be4455
arcinard <rebelo0912joao@outlook.com>
2023-09-22 00:47:52 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
 import java.util.Scanner;
-public class Main{
+
+public class Main {
         public static void main(String[] args) {
                 Scanner scanner = new Scanner(System.in);
                 int a = scanner.nextInt();
                 int b = scanner.nextInt();
                 System.out.println(a + b);
+                scanner.nextLine(); // Consume the remaining newline character
+
+                String operation = scanner.nextLine();
+
+                int result;
+                switch (operation) {
+                        case "sum":
+                                result = a + b;
+                                break;
+                        case "mul":
+                                result = a * b;
+                                break;
+                        default:
+                                System.out.println("Invalid operation");
+                                return; // Stop the program if the operation is invalid
+                }
+
+                System.out.println("Result: " + result);
         }
 }
